d27e14d168849db287d9a116378c17b8564a65ff
Ammar Qureshi <mianammarqureshi@gmail.com>
2026-06-27 14:12:27 +0100

    Fix sampling rate mismatch; add reverberation * Brings more in line with pyroomacoustics/example/room_from_rt60 with more simulation effects

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -4,10 +4,13 @@ import pyroomacoustics as pra
 from scipy.io import wavfile
 import os
 
+fs, audio = wavfile.read('data/arctic_b0540.wav') # returns rate, data 
 room_dim = [10, 10, 10] # metres
-room = pra.ShoeBox(room_dim)
 
-_, audio = wavfile.read('data/arctic_b0540.wav') # returns rate, data 
+rt60 = 0.5
+e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
+
+room = pra.ShoeBox(room_dim, fs=fs, materials=pra.Material(e_absorption), max_order=max_order, use_rand_ism=True, air_absoption=True)
 
 room.add_source([5,5,5], signal=audio)
 
